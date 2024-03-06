7111ae15bfa3abe17358187d67b27a5487f15857
Taghlaoui Oussama <oussamataghlaoui20@gmail.com>
2024-03-06 19:57:43 +0100

    Add questions and answers to the quiz component.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
+import QUESTIONS from "./../questions";
 
 function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
-  return <p>Currently Active Question</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
+  };
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h1>{QUESTIONS[activeQuestionIndex].text}</h1>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+            <li key={index} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Quiz;
